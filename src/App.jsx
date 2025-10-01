d1c18a11768a9b5f9741d1dd3592964d5e350da4
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-10-01 13:09:33 +0200

    Creazione useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ const initialMovies = [
   { title: "Batman", genre: "Azione" },
   { title: "Interstellar", genre: "Fantascienza" },
   { title: "Pulp Fiction", genre: "Thriller" },
+  { title: "Barry Lyndon", genre: "Storico" },
 ];
 
 function App() {
@@ -17,6 +18,17 @@ function App() {
     const [selectedGenre, setSelectedGenre] = useState("");
     const [filteredMovies, setFilteredMovies] = useState(initialMovies);
     const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(()=>{
+        const result = movies
+        .filter ((movie)=> !selectedGenre || movie.genre === selectedGenre)
+
+        .filter((movie) => !searchTerm || movie.title.toLowerCase().includes(searchTerm.toLowerCase())  
+     );
+     setFilteredMovies(result);
+
+     },[movies, selectedGenre, searchTerm]);
+
 
   return (
     <>
