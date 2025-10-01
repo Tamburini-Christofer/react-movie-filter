be60392a19b0f752d9537597dcf1b5e66d766c0d
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-10-01 13:03:32 +0200

    Creazione state + array film

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { useState,useEffect } from 'react'
+
 import './App.css'
+
+const initialMovies = [
+  { title: "Inception", genre: "Fantascienza" },
+  { title: "Il Padrino", genre: "Thriller" },
+  { title: "Titanic", genre: "Romantico" },
+  { title: "Batman", genre: "Azione" },
+  { title: "Interstellar", genre: "Fantascienza" },
+  { title: "Pulp Fiction", genre: "Thriller" },
+];
 
 function App() {
 
+    const [movies, setMovies] = useState(initialMovies);
+    const [selectedGenre, setSelectedGenre] = useState("");
+    const [filteredMovies, setFilteredMovies] = useState(initialMovies);
+    const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <>
-      
+
     </>
   )
 }
